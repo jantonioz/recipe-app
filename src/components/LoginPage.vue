9fc2eb811ee4c279f0e0b2c8d0198e620bc93f63
antoniozandate <antonio.zandate@metricamovil.com>
2021-01-12 21:12:18 -0600

    fix label

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,7 +4,7 @@
 			<v-flex xs12 sm8 md4>
 				<v-card class="elevation-12">
 					<v-toolbar dark color="primary">
-						<v-toolbar-title>Credenciales</v-toolbar-title>
+						<v-toolbar-title>Credentials</v-toolbar-title>
 					</v-toolbar>
 					<v-card-text>
 						<v-alert dense outlined type="error" v-if="error">
